1a35861f820ff7acf23fbd909ab1dd4dc6b3dfd6
Ray <i@r-ay.cn>
2021-02-04 00:12:27 +0800

    Initial commit

---
+++ ReadMe.md
@@ -0,0 +1,13 @@
+### A Simple WinX Toast Notification Tool
+
+### Preview
+
+### Button 
+
+![btn1](Resources/2.png?raw=true)  
+![btn1](Resources/22.png?raw=true)
+
+### TextInput
+
+![btn1](Resources/3.png?raw=true)  
+![btn1](Resources/32.png?raw=true)
---
+++ ToastHelper/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using ToastCore.Notification;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Diagnostics;
+using ZXing;
+using System.Drawing;
+using System.IO;
+
+namespace ToastHelper {
+    public partial class MainWindow : Window {
+        private ToastManager _manager;
+        private Action _notify;
+        private readonly IRunningObjectTable rot;
+
+        [DllImport("Ole32.dll")]
+        static extern int CreateClassMoniker([In] ref Guid rclsid,
+            out IMoniker ppmk);
+
+        [DllImport("Ole32.dll")]
+        public static extern void GetRunningObjectTable(
+           int reserved,
+           out IRunningObjectTable pprot);
+
+        [DllImport("Ole32.dll")]
+        static extern int CoRegisterClassObject(
+           [MarshalAs(UnmanagedType.LPStruct)] Guid rclsid,
+           [MarshalAs(UnmanagedType.IUnknown)] object pUnk,
+           uint dwClsContext,
+           uint flags,
+           out uint lpdwRegister);
+
+        public MainWindow() {
+            InitializeComponent();
+            _manager = new ToastManager();
+            _manager.Init<ToastManager>("ClipBoard Qr Helper");
+            ToastManager.ToastCallback += ToastManager_ToastCallback;
+            GetRunningObjectTable(0, out this.rot);
+        }
+
+        private void ToastManager_ToastCallback(string app, string arg, List<KeyValuePair<string, string>> kvs) {
+            App.Current.Dispatcher.Invoke(() => {
+                if (arg.StartsWith("copy:")) Clipboard.SetText (arg.Substring(5));
+                if (arg.StartsWith("goUrl:")) Process.Start("explorer.exe", arg.Substring(6));
+            });
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _notify = new Action(() => _manager.Notify("Hi","World"));
+            _notify?.BeginInvoke(null, null);
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            var windowClipboardManager = new ClipboardManager(this);
+            windowClipboardManager.ClipboardChanged += ClipboardChanged;
+        }
+
+        private void ClipboardChanged(object sender, EventArgs e)
+        {
+            if (Clipboard.ContainsImage())
+            {
+                Bitmap image = BitmapFromSource(Clipboard.GetImage());
+                IBarcodeReader reader = new BarcodeReader();
+                var barcodeBitmap = image;
+                var result = reader.Decode(barcodeBitmap);
+                if (result != null)
+                {
+                    if (result.Text.ToLower().StartsWith("http://") || result.Text.ToLower().StartsWith("https://"))
+                    {
+                        _notify = new Action(() => _manager.Notify("从剪贴板图片中读取到了二维码信息", result.Text, new ToastCommands[] { new ToastCommands { Content = "复制", Argument = "copy:" + result.Text } }, new ToastCommands[] { new ToastCommands { Content = "前往", Argument = "goUrl:" + result.Text } })); ; ;
+                    }
+                    else
+                    {
+                        _notify = new Action(() => _manager.Notify("从剪贴板图片中读取到了二维码信息", result.Text, new ToastCommands[] { new ToastCommands { Content = "复制", Argument = "copy:" + result.Text } }, new ToastCommands[] { new ToastCommands { Content = "忽略", Argument = "nothing"} })); ; ;
+                    }
+                    _notify?.BeginInvoke(null, null);
+                }
+            }
+        }
+
+        private System.Drawing.Bitmap BitmapFromSource(BitmapSource bitmapsource)
+        {
+            System.Drawing.Bitmap bitmap;
+            using (MemoryStream outStream = new MemoryStream())
+            {
+                BitmapEncoder enc = new BmpBitmapEncoder();
+                enc.Frames.Add(BitmapFrame.Create(bitmapsource));
+                enc.Save(outStream);
+                bitmap = new System.Drawing.Bitmap(outStream);
+            }
+            return bitmap;
+        }
+    }
+}
